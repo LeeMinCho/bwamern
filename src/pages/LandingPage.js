032472ced3a74ac5fd8b8d476cd3d78d9bbdf869
LeeMinCho <ekayunan98@gmail.com>
2021-11-07 20:19:00 +0700

    Categories in LandingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Header from '../parts/Header'
 import Hero from '../parts/Hero'
 import MostPicked from '../parts/MostPicked'
-
+import Categories from '../parts/Categories'
 
 import landingPage from '../json/landingPage.json'
 
@@ -16,6 +16,7 @@ export default class LandingPage extends Component {
             <Header {...this.props}></Header>
             <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
             <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
+            <Categories data={landingPage.categories} />
         </>
     }
 }
